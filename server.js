d69f1b2de0c7254be2235c58e0290fcdbcfb7e3e
Khoi Phan <jasonphan2631@gmail.com>
2024-02-29 22:12:52 -0800

    change code to heroku

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
+  
   },
   resave: false,
   saveUninitialized: true,
