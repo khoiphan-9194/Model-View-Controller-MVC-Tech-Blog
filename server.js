6a45d1826dd6e5551a8f74a9b501892876adc772
Khoi Phan <jasonphan2631@gmail.com>
2024-02-29 23:01:09 -0800

    ew

--- server.js
+++ server.js
@@ -14,13 +14,8 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
+  secret: "Super secret secret",
+  cookie: { maxAge: 7200000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
