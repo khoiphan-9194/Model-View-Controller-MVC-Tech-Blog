dd7778a178a14a8982128a2af81c96652d04307d
Khoi Phan <jasonphan2631@gmail.com>
2024-02-29 22:19:17 -0800

    222

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,21 @@
-// Dotenv import
-require("dotenv").config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-// Sequelize import
-const Sequelize = require("sequelize");
+let sequelize;
 
-// Allows for environmental variables to be used
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: "localhost",
-      dialect: "mysql",
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
-// Sequelize export
 module.exports = sequelize;
