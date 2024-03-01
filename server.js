cc31a39e94d180efe201f83446cb16ad0a945dba
Khoi Phan <jasonphan2631@gmail.com>
2024-02-29 22:25:22 -0800

    qwq

--- server.js
+++ server.js
@@ -37,10 +37,10 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join('public')));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(process.env.PORT || 3001, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
