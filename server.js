4cc6f9e3c957dcb73f6c7e81e180af699263a5a8
Khoi Phan <jasonphan2631@gmail.com>
2024-02-29 22:16:06 -0800

    123

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-  
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
   },
   resave: false,
   saveUninitialized: true,
@@ -34,7 +37,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join('public')));
 
 app.use(routes);
 
